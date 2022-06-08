5d6a13dc0dfdd997c485aa163b839a00063212b5
tduncan14 <nbatre1@yahoo.com>
2022-06-07 20:31:28 -0700

    keep moving forward

--- src/components/home/hero/Hero.js
+++ src/components/home/hero/Hero.js
@@ -13,10 +13,23 @@ const Hero = () => {
      <section className="hero">
           <div className="container">
                <div className="row">
-                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam dolorum ducimus ratione autem! Tempore voluptatibus cumque, alias molestiae iusto omnis ex corporis facilis quibusdam quod maxime dolorum, unde asperiores quo.</p>
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam dolorum ducimus ratione autem! Tempore voluptatibus cumque, alias molestiae iusto omnis ex corporis facilis quibusdam quod maxime dolorum, unde asperiores quo.</p>
+                      <div className="button">
+                   <button className="primary-btn">
+                       GET STARTED NOW <i className="fa fa-long-arrow-alt-right"></i>
+                   </button>
+                   
+
+                   <button className="primary-btn">
+                       View Course<i className="fa fa-long-arrow-alt-left"></i>
+                   </button>
+                   </div>
                </div>
           </div>
      </section>
+
+
+     <div className="margin"></div>
      </>
     )
 }
